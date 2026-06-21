c803127413f0f05be14f1f7aeb39adb57f6a69d7
Nicky Bourdette (Hp) <inbourdette.est@gmail.com>
2026-06-20 21:54:34 -0300

    ejercicio 1: completo y optimizado en un solo menu de operaciones

--- ToDo/Program.cs
+++ ToDo/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Security.AccessControl;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.AccessControl;
 using EspacioTarea;
 
 /*
@@ -20,25 +22,30 @@ List<Tarea> ListaVacia()
     return new List<Tarea>();
 }
 
-void AgregarTarea ( List<Tarea> lista, Tarea t)
-{
-    lista.Add(t);
-}
-
-void QuitarTarea ( List<Tarea> lista, Tarea t)
-{
-    lista.Remove(t);
-}
-
 void CargarTarea ( List<Tarea> lista, int id, string descrip, int durac)
 {
     Tarea tareaNueva = new Tarea( id , descrip , durac );
-    AgregarTarea(lista, tareaNueva);
+    lista.Add(tareaNueva);
+}
+
+
+/* 2. Cree aleatoriamente N tareas y guardelas en una lista dedicada a tareas 
+pendientes (por ejemplo, List<Tarea> tareasPendientes).  */
+
+void CargarListaPendientes (List<Tarea> listaPendientes, int n)
+{
+    Random rnd = new Random(); // defino un objeto 'rnd' tipo 'Random' para generar numeros aleatorios
+    // revisar el archivo Readme.md para más info de cómo usar esta función.
+
+    for (int i = 1; i <= n; i++)
+    {
+        CargarTarea ( listaPendientes, i , "tarea "+ i , rnd.Next(10,101) );
+    }
 }
 
 void MostrarTarea (Tarea t)
 {
-    Console.WriteLine("\n--------------------");
+    Console.WriteLine("--------------------");
     Console.WriteLine($"Tarea ID: [{t.TareaId}]");
     Console.WriteLine($"Descripción: \'{t.Descripcion}\'");
     Console.WriteLine($"Duración: {t.Duracion} días");
@@ -56,119 +63,182 @@ void MostrarLista (string tipoLista, List<Tarea> listaTareas)
 
 bool MoverTareaAListaRealizadas(List<Tarea> listaPendientes, List<Tarea> listaRealizadas, Tarea tareaAMover)
 {
-    //Tarea? tareaAMover = listaPendientes.FirstOrDefault(t => t.TareaId == id);
-    QuitarTarea(listaPendientes, tareaAMover);
-    AgregarTarea(listaRealizadas, tareaAMover);
+    listaPendientes.Remove(tareaAMover);
+    listaRealizadas.Add(tareaAMover);
     if ( listaRealizadas.Contains(tareaAMover) && !listaPendientes.Contains(tareaAMover) )
     { return true; }
     else
     { return false; }
 }
 
-bool BuscarTarea (List<Tarea> listaPendientes, string tBuscada)
+Tarea BuscarTarea (List<Tarea> listaPendientes, string tBuscada)
 {
     foreach (Tarea t in listaPendientes)
     {
         if (t.Descripcion == tBuscada)
         {
-            MostrarTarea(t);
-            Console.WriteLine("\nEstado: PENDIENTE");
-            return true;
+            return t;
         }
     }
-    return false;
+    return null;
 }
 
 
-//
+/* 3. Desarrolle una interfaz para mover las tareas pendientes a realizadas. 
+    La lista de tareas realizadas (por ejemplo, List<Tarea> tareasRealizadas) 
+    inicialmente estará vacía.  */
 
-/* 2. Cree aleatoriamente N tareas y guardelas en una lista dedicada a tareas 
-pendientes (por ejemplo, List<Tarea> tareasPendientes).  */
-
-int n = 5;
-List<Tarea> tareasPendientes = ListaVacia();
-Random rnd = new Random(); // defino un objeto 'rnd' tipo 'Random' para generar numeros aleatorios
-// revisar el archivo Readme.md para más info de cómo usar esta función.
-
-for (int i = 1; i <= n; i++)
+void Operacion_MarcarTareaComoRealizada (List<Tarea> listaPendientes, List<Tarea> listaRealizadas, int idLeido)
 {
-    // Tarea tareaNueva = new Tarea( i , "tarea "+ i , rnd.Next(10,101) );
-    // tareasPendientes.Add(tareaNueva);
-    CargarTarea ( tareasPendientes, i , "tarea "+ i , rnd.Next(10,101) );
+    Tarea? tareaAMover = listaPendientes.FirstOrDefault(t => t.TareaId == idLeido);
+    if (tareaAMover != null)
+    {
+        if ( MoverTareaAListaRealizadas(listaPendientes, listaRealizadas, tareaAMover) )
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\n>>> La tarea fue movida con éxito <<<");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            MostrarLista ("REALIZADAS", listaRealizadas);
+            Console.ResetColor();
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("\nOCURRIÓ UN ERROR AL MOVER LA TAREA");
+            Console.ResetColor();
+        }
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine("\nLa tarea no se encuentra en 'Lista-Pendientes'.");
+        Console.ResetColor();
+    }
 }
 
-if (tareasPendientes.Count() == n)
+/* 4. Desarrolle una función para buscar tareas pendientes por descripción y 
+    mostrarla por consola.  */
+
+void Operacion_BuscarTareaPorDescripcionEnPendientes (List<Tarea> listaPendientes, string tareaBuscada)
+{
+    Tarea? tareaEncontrada = BuscarTarea(listaPendientes, tareaBuscada);
+    if ( tareaEncontrada != null )
+    {
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        MostrarTarea(tareaEncontrada);
+        Console.WriteLine("\nEstado: PENDIENTE");
+        Console.ResetColor();
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine($"\nNo se ha encontrado la tarea '{tareaBuscada}' en PENDIENTES.");
+        Console.ResetColor();
+    }
+}
+
+
+/* 5. Mostrar un listado de todas las tareas (pendientes y realizadas)  */
+
+void Operacion_MostrarTodasLasTareas (List<Tarea> listaPendientes, List<Tarea> listaRealizadas)
+{
+    Console.WriteLine("\n********************");
+    MostrarLista("PENDIENTES", listaPendientes);
+    Console.WriteLine("********************");
+    MostrarLista("REALIZADAS", listaRealizadas);
+    Console.WriteLine("********************\n");
+}
+
+
+
+// CODIGO RPINCIPAL
+
+int cantidadTareas = 5;
+List<Tarea> tareasPendientes = ListaVacia();
+CargarListaPendientes(tareasPendientes, cantidadTareas);
+
+if (tareasPendientes.Count() == cantidadTareas)
 {
     Console.WriteLine("\nLista de Tareas Pendientes cargada con éxito.");
-
-    //MostrarLista ("PENDIENTES", tareasPendientes);
-        
+    
     //
     Console.ForegroundColor = ConsoleColor.DarkBlue;
     Console.WriteLine("\nPRESIONE UNA TECLA PARA CONTINUAR:");
     Console.ReadKey();
     //
 
-
-    /* 3. Desarrolle una interfaz para mover las tareas pendientes a realizadas. 
-    La lista de tareas realizadas (por ejemplo, List<Tarea> tareasRealizadas) 
-    inicialmente estará vacía.  */
-
-    List<Tarea> tareasRealizadas = ListaVacia();
-    int seguir = 1;
-    string? leer;
-    int idLeido;
     Console.ForegroundColor = ConsoleColor.DarkMagenta;
-    Console.WriteLine("\n<<<< INTERFAZ DE USUARIO >>>>\n");
+    Console.WriteLine("\n<<<< INICIO DEL PROGRAMA >>>>\n");
     Console.ResetColor();
+    
+    List<Tarea> tareasRealizadas = ListaVacia();
+
+    string? leer;
+    int operacion;
     do
     {
-        Console.ForegroundColor = ConsoleColor.DarkYellow;
-        MostrarLista ("PENDIENTES", tareasPendientes);
-        Console.ResetColor();
-        Console.WriteLine("\n¿Marcar tarea como REALIZADA? (SI: X | NO: 0): ");
+        //
+        Console.WriteLine("\nMENU de Operaciones: [elegir una opcion]");
+        Console.WriteLine("1. Marcar tarea como 'REALIZADA'");
+        Console.WriteLine("2. Buscar tarea pendiente por descripción");
+        Console.WriteLine("3. Mostrar listado de todas las tareas");
+        Console.WriteLine("0. Finalizar");
+
+        Console.WriteLine("\nOpcion: ");
         leer = Console.ReadLine();
-        if (int.TryParse(leer, out seguir) && seguir != 0)
+        if (int.TryParse(leer, out operacion) && operacion != 0)
         {
-            Console.WriteLine("\nIngresar el numero de la TAREA a mover:");
-            leer = Console.ReadLine();
-            if (int.TryParse(leer, out idLeido))
+            switch (operacion)
             {
-                Tarea? tareaAMover = tareasPendientes.FirstOrDefault(t => t.TareaId == idLeido);
-                if (tareaAMover != null)
-                {
-                    if ( MoverTareaAListaRealizadas(tareasPendientes, tareasRealizadas, tareaAMover) )
+                case 1:
+
+                    int idLeido;
+                    Console.WriteLine("\nIngresar el numero de la TAREA a mover:");
+                    leer = Console.ReadLine();
+                    if (int.TryParse(leer, out idLeido))
                     {
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine("\n>>> La tarea fue movida con éxito <<<");
-                        Console.ForegroundColor = ConsoleColor.Cyan;
-                        // tareasRealizadas.Sort();
-                        MostrarLista ("REALIZADAS", tareasRealizadas);
-                        Console.ResetColor();
+                        Operacion_MarcarTareaComoRealizada(tareasPendientes, tareasRealizadas, idLeido);
                     }
                     else
                     {
-                        Console.ForegroundColor = ConsoleColor.Magenta;
-                        Console.WriteLine("\nOCURRIÓ UN ERROR AL MOVER LA TAREA");
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("\nEl valor ingresado no es válido\n");
                         Console.ResetColor();
                     }
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Magenta;
-                    Console.WriteLine("\nLa tarea no se encuentra en 'Lista-Pendientes'.");
-                    Console.ResetColor();
-                }
+                    break;
+
+                case 2:
+                    
+                    Console.WriteLine("\nEscriba la descripcion de la tarea para buscar: ");
+                    string? tareaBuscada = Console.ReadLine();
+                    if ( !String.IsNullOrEmpty(tareaBuscada) )
+                    {
+                        Operacion_BuscarTareaPorDescripcionEnPendientes(tareasPendientes, tareaBuscada);
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("\nEl valor ingresado no es válido\n");
+                        Console.ResetColor();
+                    }
+                    break;
+
+                case 3:
+
+                    Operacion_MostrarTodasLasTareas(tareasPendientes, tareasRealizadas);
+                    break;
+
+                default:
+                    break;
             }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("\nEl valor ingresado no es válido\n");
-                Console.ResetColor();
-            }
+            //
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("\nPRESIONE UNA TECLA PARA CONTINUAR...");
             Console.ReadKey();
+            Console.ResetColor();
         }
-    } while(seguir != 0);
+
+    } while(operacion != 0);
 
     Console.ForegroundColor = ConsoleColor.DarkMagenta;
     Console.WriteLine("\n---> FIN DEL PROGRAMA 1 <---\n");
@@ -177,58 +247,8 @@ if (tareasPendientes.Count() == n)
     Console.ForegroundColor = ConsoleColor.DarkBlue;
     Console.WriteLine("\nPRESIONE UNA TECLA PARA CONTINUAR:");
     Console.ReadKey();
-    //
-
-    /* 4. Desarrolle una función para buscar tareas pendientes por descripción y 
-    mostrarla por consola.  */
-
-    Console.ForegroundColor = ConsoleColor.DarkMagenta;
-    Console.WriteLine("\n>>> SEGUNDO PROGRAMA <<<\n");
-    Console.ResetColor();
-    Console.WriteLine("\nEscriba la descripcion de la tarea para buscar: ");
-    string? tareaBuscada = Console.ReadLine();
-    if ( !String.IsNullOrEmpty(tareaBuscada) )
-    {
-        bool encontrada = BuscarTarea(tareasPendientes, tareaBuscada);
-        if ( !encontrada )
-        {
-            Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.WriteLine($"\nNo se ha encontrado la tarea '{tareaBuscada}' en PENDIENTES.");
-            Console.ResetColor();
-        }
-    }
-    else
-    {
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine("\nEl valor ingresado no es válido\n");
-        Console.ResetColor();
-    }
-
-    Console.ForegroundColor = ConsoleColor.DarkMagenta;
-    Console.WriteLine("\n---> FIN DEL PROGRAMA 2 <---\n");
-    //
-    Console.ForegroundColor = ConsoleColor.DarkBlue;
-    Console.WriteLine("\nPRESIONE UNA TECLA PARA CONTINUAR:");
-    Console.ReadKey();
     Console.ResetColor();
     //
-    
-    /* 5. Mostrar un listado de todas las tareas (pendientes y realizadas)  */
-
-    MostrarLista("PENDIENTES", tareasPendientes);
-    MostrarLista("REALIZADAS", tareasRealizadas);
-
-    //
-    Console.ForegroundColor = ConsoleColor.DarkBlue;
-    Console.WriteLine("\nPRESIONE UNA TECLA PARA CONTINUAR:");
-    Console.ReadKey();
-    //
-
-    /* 6. Diseña un menú principal que permita al usuario acceder a cada una de las funcionalidades descritas. 
-    La interacción debe ser intuitiva (ej. "Presione 1 para...", "Ingrese el ID de la 
-    tarea:", etc.).  */
-
-
 }
 else
 {
